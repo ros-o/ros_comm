70ab83221fd397b6287ccd3fc41ffca41a28f339
Lucas Walter <wsacul@gmail.com>
2023-12-23 08:44:52 -0800

    massive search and replace of deprecated assertEquals -> assertEqual

--- tools/rostest/src/rostest/runner.py
+++ tools/rostest/src/rostest/runner.py
@@ -179,8 +179,8 @@ def rostestRunner(test, test_pkg, results_base_dir=None):
                     printlog("test [%s] results summary: %s errors, %s failures, %s tests",
                              test_name, results.num_errors, results.num_failures, results.num_tests)
 
-                    #self.assertEquals(0, results.num_errors, "unit test reported errors")
-                    #self.assertEquals(0, results.num_failures, "unit test reported failures")
+                    #self.assertEqual(0, results.num_errors, "unit test reported errors")
+                    #self.assertEqual(0, results.num_failures, "unit test reported failures")
             else:
                 if test.retry:
                     printlogerr("retry is disabled in --text mode")
